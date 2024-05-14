b988930aa9f9c6422921b09661f02668796043c0
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-05-14 11:53:53 +0800

    minor amendments

--- app.py
+++ app.py
@@ -14,9 +14,28 @@ def home():
     <title>Index</title>
 </head>
 <body>
-    <main class="bg-black mx-auto text-center min-h-screen min-w-full">
+    <main class="bg-black mx-auto text-center min-h-screen min-w-full p-2">
         <h1 class="text-4xl text-pink-500">Hello world</h1>
     </main>
 </body>
 </html>
     '''
+
+@app.route("/test")
+def test():
+    return '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Test</title>
+</head>
+<body>
+    <main class="bg-black mx-auto text-center min-h-screen min-w-full p-2">
+        <h1 class="text-4xl text-pink-500">Testing testing 123...</h1>
+    </main>
+</body>
+</html>
+'''
